c899392b94c87825df6610e6f164b4b116cc7dd1
Sławomir Zaraziński <jestemKoderem@gmail.com>
2020-03-19 21:03:45 +0100

    Introduce working directory

--- dataset/start.sh
+++ dataset/start.sh
@@ -1,17 +1,18 @@
 [[ -z "$LEGO_HOME" ]] && { echo "LEGO_HOME is empty" ; exit 1; }
 
-# shellcheck disable=SC2164
-cd "$LEGO_HOME" 
+WORKING_DIRECTORY="$LEGO_HOME/dataset"
+
+cd "$WORKING_DIRECTORY" || exit
 
 ## install blender
 yes | sudo apt-get install snapd xdg-open-snapd
 yes | sudo snap install blender --classic
 
 ## clone ldraw importer add-on
-git clone https://github.com/TobyLobster/ImportLDraw.git
+git clone -b master --single-branch https://github.com/TobyLobster/ImportLDraw.git
 
 ## create path to add-on
-ADDON_PATH="$LEGO_HOME/ImportLDraw"
+ADDON_PATH="$WORKING_DIRECTORY/ImportLDraw"
 
 ## install add-on
 blender --background --python install_addon.py -- "$ADDON_PATH" "importldraw"
@@ -21,4 +22,4 @@ wget -nc http://www.ldraw.org/library/updates/complete.zip
 unzip complete.zip
 
 ## run blender
-blender --background --addons importldraw --python render.py -- "$LEGO_HOME/ldraw/parts/1.dat" "$LEGO_HOME/test_render/1.png" "$LEGO_HOME/ldraw"
+blender --background --addons importldraw --python render.py -- "$WORKING_DIRECTORY/ldraw/parts/1.dat" "$WORKING_DIRECTORY/test_render/1.png" "$WORKING_DIRECTORY/ldraw"
