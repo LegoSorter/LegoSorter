e74463b1bb0a57a1f01b4ec5caaa3a508ac2f396
Sławomir Zaraziński <jestemKoderem@gmail.com>
2020-03-19 20:25:30 +0100

    Add a starting script
    This script set up an environment - downloads and install blender with an importer addon and renders one image.

---
+++ dataset/start.sh
@@ -0,0 +1,24 @@
+[[ -z "$LEGO_HOME" ]] && { echo "LEGO_HOME is empty" ; exit 1; }
+
+# shellcheck disable=SC2164
+cd "$LEGO_HOME" 
+
+## install blender
+yes | sudo apt-get install snapd xdg-open-snapd
+yes | sudo snap install blender --classic
+
+## clone ldraw importer add-on
+git clone https://github.com/TobyLobster/ImportLDraw.git
+
+## create path to add-on
+ADDON_PATH="$LEGO_HOME/ImportLDraw"
+
+## install add-on
+blender --background --python install_addon.py -- "$ADDON_PATH"
+
+## download ldraw parts
+wget http://www.ldraw.org/library/updates/complete.zip
+unzip complete.zip
+
+## run blender
+blender --background --addons importldraw --python render.py -- "$LEGO_HOME/ldraw/parts/1.dat" "$LEGO_HOME/output/1.png" "$LEGO_HOME/ldraw"
